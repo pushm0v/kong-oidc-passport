14161ad405e01abe69be7469dd0c1cc331ee8ef7
Bherly Novrandy <pushm0v.development@gmail.com>
2023-07-26 16:51:47 +0700

    Add introspect endpoint

--- bwi-passport/routes/api.php
+++ bwi-passport/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('api')->group(function() {
+    Route::post('/oauth/introspect',
+        [\App\Http\Controllers\PassportIntrospectController::class, 'introspect']);
+});
